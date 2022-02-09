093bcb9b098e5e361d2908d15265db71ce9b750d
李国冬 <liguodongiot@163.com>
2022-02-09 16:54:00 +0800

    代码格式化工具及代码风格检测工具

---
+++ scaffold-domain/src/main/java/org/lgd/scaffold/domain/UserInfo.java
@@ -0,0 +1,25 @@
+package org.lgd.scaffold.domain;
+
+import lombok.Data;
+
+/**
+ * <p>
+ * Project: springboot-project-scaffold
+ * Package: org.lgd.scaffold.domain
+ * Title: UserInfo
+ * Description: UserInfo
+ * </p>
+ *
+ * @author liguodong
+ * @version 1.0.0
+ * @date 2022/2/9
+ */
+
+@Data
+public class UserInfo {
+
+private String name;
+
+        private Integer age;
+
+}
